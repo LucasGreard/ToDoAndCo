f55ff92a89aaa93690eb207d760bcc0e23d70a68
LucasGreard <86890031+LucasGreard@users.noreply.github.com>
2022-02-04 10:27:26 +0100

    Test

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,6 +13,7 @@ class HomeController extends AbstractController
      */
     public function index(): Response
     {
+        $x = 1;
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);

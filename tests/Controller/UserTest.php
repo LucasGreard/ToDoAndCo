c61822883118572765d7eed86a83d6e562cda9be
LucasGreard <86890031+LucasGreard@users.noreply.github.com>
2022-02-22 13:11:45 +0100

    Add new test, increase Coverage

--- tests/Controller/UserTest.php
+++ tests/Controller/UserTest.php
@@ -90,35 +90,27 @@ class UserTest extends WebTestCase
         $userRepository = static::getContainer()->get(UserRepository::class);
         $testUser = $userRepository->findOneBy(["email" => $user['email']]);
 
-        $crawler = $client->request('GET', '/user/' . $testUser->getId() . '/edit');
+        $client->request('GET', '/user/' . $testUser->getId() . '/edit');
 
         $this->assertResponseIsSuccessful();
         $this->assertInputValueSame('user_edit[email]', $user['email']);
     }
-    // public function testUserDelete()
-    // {
-    //             // $client = $this->createLoginForTest();
-    //     // $client->followRedirects();
-    //     // $crawler = $client->request('GET', '/user/new');
-    //     // $form = $crawler->selectButton('Save')->form();
-    //     // $email = 'anaelleoury40@gmail.com' . rand(1, 100);
-    //     // $email = 'anaelleoury40@gmail.com20';
-    //     // $form['user[email]']->setValue($email);
-    //     // $form['user[plainPassword]']->setValue('prout50');
-    //     // $form['user[roles]']->setValue('ROLE_USER');
-    //     // $tkn = new CsrfToken("23", "delete");
-    //     // $form['user[_token]']->setValue($tkn->__toString());
+    public function testUserDelete()
+    {
+        $user = $this->createUser();
+        $client = $this->createLoginForTest();
+        $client->followRedirects();
 
-    //     // $client->submit($form);
-    //     // $crawler = $client->followRedirects();
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        $testUser = $userRepository->findOneByEmail($user['email']);
+        $crawler = $client->request('GET', '/user/' . $testUser->getId());
 
-    //     // $userRepository = static::getContainer()->get(UserRepository::class);
-    //     // $testUser = $userRepository->findOneByEmail($email);
+        $form = $crawler->selectButton('Delete')->form();
+        $client->submit($form);
 
-    //     // $crawler = $client->request('POST', '/user/23');
-    //     // $crawler = $client->followRedirects();
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        $testUser = $userRepository->findOneByEmail($user['email']);
 
-
-    //     // $this->assertEquals(1, 1);
-    // }
+        $this->assertNull($testUser);
+    }
 }
